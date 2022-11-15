d53f219b164f3f6d7b095897bec26c7e378f1b81
Rishi Vamshi <rishivamshi0079@gmail.com>
2022-11-15 01:24:14 -0500

    Solved reverse linked list using recursion

--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -4,17 +4,31 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    
+    
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        reverse = []
-        current = head
-        while(head):
-            reverse.append(head.val)
-            head = head.next
-        reverse = reverse[::-1]
-        i = 0
-        head = current
-        while(head):
-            head.val = reverse[i]
-            i+=1
-            head=head.next
-        return current
+        # reverse = []
+        # current = head
+        # while(head):
+        #     reverse.append(head.val)
+        #     head = head.next
+        # reverse = reverse[::-1]
+        # i = 0
+        # head = current
+        # while(head):
+        #     head.val = reverse[i]
+        #     i+=1
+        #     head=head.next
+        # return current
+        
+        # using recursion
+        
+        if head==None or head.next == None:
+            return head
+        
+        ans = self.reverseList(head.next)
+        k = head.next
+        k.next = head
+        head.next = None
+        return ans
+
