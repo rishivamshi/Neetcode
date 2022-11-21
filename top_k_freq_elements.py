c1c569a67580acc51d98ccc946c631f534f1fc47
Rishi Vamshi <rishivamshi0079@gmail.com>
2022-11-21 16:29:56 -0500

    solved top k freq elements using a bucket sort implementation - my code

--- top_k_freq_elements.py
+++ top_k_freq_elements.py
@@ -16,3 +16,45 @@ class Solution:
             ans.append(q.get()[1])
             k-=1
         return ans
+
+from queue import PriorityQueue
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        d = {}
+        # adding number into dictionary
+        for num in nums:
+            if num in d:
+                d[num]+=1
+            else:
+                d[num] = 1
+        #finding the highest most repeated element to get the size of the array
+        maxRepeat = 0
+        for key in d:
+            if d[key]>=maxRepeat:
+                maxRepeat = d[key]
+        # print(maxRepeat)
+        # creating a dictionary of values to key and key to values of d
+        d1= defaultdict(list)
+        for key in d:
+            if d[key] in d1:
+                d1[d[key]] = d1[d[key]] + [key]
+            else:
+                d1[d[key]] = [key]
+        # print(d1)
+        
+        #initiating the final sorted array of bucket sort
+        n = [[]]*(maxRepeat+1)
+        for key in d1:
+            n[key] = d1[key]
+        ans = []
+        print(n)
+        count = 0
+        # to append and return the final answer
+        for i in range(len(n)-1,0,-1):
+            if count==k:
+                break
+            for j in range(len(n[i])):
+                ans.append(n[i][j])
+                count+=1
+
+        return ans
